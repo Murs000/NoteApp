cff47f776bcf68bf812f03075c7ddb8506530ec5
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-09-15 19:21:54 +0400

    App. Email Hotfix.

--- NoteApp/Services/EmailService.cs
+++ NoteApp/Services/EmailService.cs
@@ -6,10 +6,10 @@ namespace NoteApp.Services;
 
 public class EmailService
 {
-    private readonly string _smtpServer = "smtp.your-email-provider.com"; // Configure your SMTP server
+    private readonly string _smtpServer = "smtp.gmail.com"; // Configure your SMTP server
     private readonly int _smtpPort = 587; // Port number (e.g., 587 for TLS)
-    private readonly string _smtpUser = "your-email@example.com"; // SMTP username
-    private readonly string _smtpPass = "your-email-password"; // SMTP password
+    private readonly string _smtpUser = "m.mastali7@gmail.com"; // SMTP username
+    private readonly string _smtpPass = "odjm vapt mgod dzgu"; // SMTP password
 
     public async Task SendConfirmationEmailAsync(string email, string token)
     {
@@ -17,7 +17,7 @@ public class EmailService
         {
             From = new MailAddress(_smtpUser),
             Subject = "Confirm your email",
-            Body = $"Please confirm your email by clicking <a href='https://yourdomain.com/Account/ConfirmEmail?email={email}&token={token}'>here</a>.",
+            Body = $"Please confirm your email by clicking <a href='http://localhost:5299/Account/ConfirmEmail?email={email}&token={token}'>here</a>.",
             IsBodyHtml = true
         };
         mailMessage.To.Add(email);
