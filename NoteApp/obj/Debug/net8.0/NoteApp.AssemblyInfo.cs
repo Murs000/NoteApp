56549a4e21cc6b9f43409b803d9bcf5577c42175
Murs000 <129412232+Murs000@users.noreply.github.com>
2024-09-22 15:10:13 +0400

    App. Impove desing of NoteDetails Page.

--- NoteApp/obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
+++ NoteApp/obj/Debug/net8.0/NoteApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c408dc2cb766e158ad682e84e37283363df6d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871cc3817a9d27002e6a9fac86e9a994c9e74c0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NoteApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
